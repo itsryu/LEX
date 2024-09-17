c9ba54f6148b8e8d3989595d3d1e7a3cb3742503
itsryu <joaovictor9162@outlook.com>
2024-09-17 01:20:43 -0300

    feat: adding .bat file to compile faster

---
+++ windows.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set dir=%~dp0
+
+cd %dir% && gcc ./lexer.c ./main.c -o main.exe
+
+if %errorlevel% equ 0 (
+    cls && start cmd /k main.exe --file ./main.pas
+    exit 0
+) else (
+    echo Erro ao compilar o programa.
+    pause
+    exit 1
+)
