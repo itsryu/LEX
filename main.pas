c29f0493611f39d67398f897d887657b06c06092
itsryu <joaovictor9162@outlook.com>
2024-09-28 17:36:30 -0300

    feat: Removida declaração desnecessária de variável
    Removida a declaração da variável 's' que não estava sendo utilizada no código.

--- main.pas
+++ main.pas
@@ -3,14 +3,12 @@ program Exemplo;
 var 
    x, y: integer; 
    z: real;
-   s: string;
 
 begin
    x := 10;
    y := 20;
    z := x - y * 2.5;
-   s := 'teste';
-
+   
    if x > y then
       x := x - 1;
    else
